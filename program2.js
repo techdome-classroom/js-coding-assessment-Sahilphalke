87696e7084cc31f46453d4e59b91b264dea794e5
sahilphalke <sahilphalke2003@gmail.com>
2024-10-17 18:14:08 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,35 +1,7 @@
-const decodeTheRing = function (message, pattern) {
-  const m = message.length;
-  const n = pattern.length;
+const decodeTheRing = function (s, p) {
 
-  // Create a DP table
-  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false));
+    // write your code here
+
+  };
   
-  // Base case: both strings are empty
-  dp[0][0] = true;
-
-  // Fill the first row for patterns starting with '*'
-  for (let j = 1; j <= n; j++) {
-      if (pattern[j - 1] === '*') {
-          dp[0][j] = dp[0][j - 1]; // '*' can match empty
-      }
-  }
-
-  // Fill the DP table
-  for (let i = 1; i <= m; i++) {
-      for (let j = 1; j <= n; j++) {
-          if (pattern[j - 1] === '*') {
-              // '*' can match zero or more characters
-              dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-          } else if (pattern[j - 1] === '?' || pattern[j - 1] === message[i - 1]) {
-              // '?' matches any single character or characters are the same
-              dp[i][j] = dp[i - 1][j - 1];
-          }
-      }
-  }
-
-  // The result is in the bottom-right corner of the DP table
-  return dp[m][n];
-};
-
-module.exports = decodeTheRing;
+  module.exports = decodeTheRing;

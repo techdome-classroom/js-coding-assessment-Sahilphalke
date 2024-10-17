0e5fda29c3661e47d0ee589fc16b751a811cfa77
sahilphalke <sahilphalke2003@gmail.com>
2024-10-17 18:28:39 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,43 +1,18 @@
-const decodeTheRing = function (s, p) {
-  // Initialize pointers for the secret message and decoder key
-  let i = 0;
-  let j = 0;
+function decodeTheRing(s, p) {
+  let i = 0, j = 0;
 
-  // Iterate through both strings
   while (i < s.length && j < p.length) {
-    // If the current character in the decoder key is a star,
-    // skip all characters in the secret message until a mismatch or the end
     if (p[j] === '*') {
-      while (i < s.length && j < p.length && p[j] === '*') {
-        j++;
-      }
-      // If the end of the decoder key is reached, the match is successful
-      if (j === p.length) {
-        return true;
-      }
-      // If not, continue matching the remaining characters
-      continue;
-    }
-
-    // If the current character in the decoder key is a question mark,
-    // skip the current character in the secret message
-    if (p[j] === '?') {
+      while (j < p.length && p[j] === '*') j++;
+      if (j === p.length) return true;
+    } else if (p[j] === '?' || s[i] === p[j]) {
+      i++;
       j++;
-      continue;
-    }
-
-    // If the characters don't match, the match fails
-    if (s[i] !== p[j]) {
+    } else {
       return false;
     }
-
-    // Increment both pointers
-    i++;
-    j++;
   }
 
-  // If the end of both strings is reached, the match is successful
   return i === s.length && j === p.length;
-};
-  
+}
   module.exports = decodeTheRing;
